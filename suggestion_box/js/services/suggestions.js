f7f041fe78936f152dbab8f6fb5a5b73b4febcd2
DavidChen <kchen117@umd.edu>
2016-02-07 19:54:15 -0500

    Initial commit of suggestion boc

---
+++ suggestion_box/js/services/suggestions.js
@@ -0,0 +1,27 @@
+app.factory('suggestions', [function(){
+	var demoSuggestions = {
+		posts: [
+			{
+				title: 'Fried rice at Trader Joe\'s',
+				upvotes: 20,
+				comments: [],
+			},
+			{
+				title: 'Dried mango at Trader Joe\'s',
+				upvotes: 21,
+				comments: [],
+			},
+			{
+				title: "Mocha ice cream",
+				upvotes: 1,
+				comments: [],
+			},
+			{
+				title: 'Shu mai (Chicken)',
+				upvotes: 19,
+				comments: ['Pan fry is good'],
+			}
+		]
+	};
+	return demoSuggestions;
+}]);
